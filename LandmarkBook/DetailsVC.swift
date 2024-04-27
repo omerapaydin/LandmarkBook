a22e637a7d01d06bbf22b9b983d8dab74c6c52e2
Omer Apaydin <omerapaydin90@gmail.com>
2024-04-27 19:20:35 +0300

    Second

--- LandmarkBook/DetailsVC.swift
+++ LandmarkBook/DetailsVC.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class DetailsVC: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var landmarkName: UILabel!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        landmarkName.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
